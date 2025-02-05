fe9f2740d632645597fcc9d336625683f7376b90
Ville <viltsu.soi@gmail.com>
2025-02-05 10:53:42 +0200

    Add project files.

---
+++ StudentManagement/Data/StudentContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models;
+using System;
+
+namespace StudentManagement.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options) { }
+        public DbSet<Student> Students { get; set; } = null!;
+    }
+}
